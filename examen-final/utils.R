40af666727333891e6c074223396e245ab208a40
Bruno <brunocgf@gmail.com>
2020-12-13 21:27:13 -0600

    9-13

--- examen-final/utils.R
+++ examen-final/utils.R
@@ -1,5 +1,6 @@
 
 # Función para cargar defunciones
+# Por favor corregir la dirección donde se entuentran los archivos de defunciones
 cargar_defunciones <- function(path="./datos_examen/poblacion/defunciones/"){
   x <- list.files(path, full.names=TRUE, recursive = TRUE)
   def_acum <- NULL
